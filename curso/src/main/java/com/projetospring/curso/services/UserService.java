afd73595feb63eef8ec73cbc2de67d8d34e0b354
Juan Silva <juanportela100@gmail.com>
2025-02-12 03:28:04 -0300

    Exceptions delete

--- curso/src/main/java/com/projetospring/curso/services/UserService.java
+++ curso/src/main/java/com/projetospring/curso/services/UserService.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 
 import com.projetospring.curso.entidade.User;
 import com.projetospring.curso.repositories.UserRepository;
+import com.projetospring.curso.services.exceptions.DatabaseException;
 import com.projetospring.curso.services.exceptions.ResourceNotFoundException;
 
 
@@ -32,7 +35,13 @@ public class UserService  {
 	}
 	
 	public void deletar (Long id) {
+		try {
 		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User atualizar(Long id, User obj) {
