cc56567089dc8a57380adbd957813de47fa72c19
Juan Silva <juanportela100@gmail.com>
2025-02-11 23:33:24 -0300

    categorias

--- curso/src/main/java/com/projetospring/curso/config/Testconfig.java
+++ curso/src/main/java/com/projetospring/curso/config/Testconfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.projetospring.curso.entidade.Categoria;
 import com.projetospring.curso.entidade.Pedido;
 import com.projetospring.curso.entidade.User;
 import com.projetospring.curso.entidade.enums.StatusPedido;
+import com.projetospring.curso.repositories.CategoriaRepository;
 import com.projetospring.curso.repositories.PedidoRepository;
 import com.projetospring.curso.repositories.UserRepository;
 
@@ -20,6 +22,9 @@ public class Testconfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
@@ -35,10 +40,14 @@ public class Testconfig implements CommandLineRunner {
 		Pedido o2 = new Pedido (null, Instant.parse("2019-07-21T03:42:10Z"),StatusPedido.DELIVERED, u2);
 		Pedido o3 = new Pedido (null, Instant.parse("2019-07-22T15:21:22Z"),StatusPedido.WAITING_PAYMENT, u1); 
 		
-		
+		Categoria cat1 = new Categoria(null, "Electronics");
+		Categoria cat2 = new Categoria(null, "Books");
+		Categoria cat3 = new Categoria(null, "Computers"); 
+
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		pedidoRepository.saveAll(Arrays.asList(o1,o2,o3));
+	    categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 
 }
